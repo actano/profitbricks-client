3353f747105d577b725c1dbc9da81bd97f463d2c
thrucker <t.rucker@itsonix.eu>
2016-12-01 15:25:11 +0100

    add compiled js files and `index.js`
    * to avoid needing to have coffee-script installed when using this
    module

---
+++ index.js
@@ -0,0 +1,13 @@
+const DatacenterCreator = require('./lib/datacenterCreator')
+const DatacenterInspector = require('./lib/datacenterInspector')
+const DatacenterManager = require('./lib/datacenterManager')
+const DatacenterToolbox = require('./lib/tools')
+const Models = require('./lib/models')
+
+module.exports = {
+  DatacenterCreator,
+  DatacenterInspector,
+  DatacenterManager,
+  DatacenterToolbox,
+  Models,
+}
